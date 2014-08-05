0d80c325678021124a11367f15167f8296095a31
chinazaj <a0a1a2a3>
2014-08-05 20:53:51 +0800

    first import

---
+++ src/main/java/com/luzita/platform/po/SYS_PO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.luzita.platform.po;
+
+import com.luzita.platform.SYS_OBJ;
+
+/**
+ *
+ * @author lenovo
+ */
+public class SYS_PO extends SYS_OBJ{
+    
+}
